eb7262a38556ca3e7c1f8b1760c4cfe5245b57b3
Kelly An <4ever.kellyan@gmail.com>
2014-08-02 23:23:19 -0700

    add categoryTableViewCell and several images

---
+++ ios/BeaconGuide/BeaconGuide/categoryTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  categoryTableViewCell.h
+//  BeaconGuide
+//
+//  Created by Kaili An on 8/2/14.
+//  Copyright (c) 2014 Beacon_Guide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface categoryTableViewCell : UITableViewCell
+
+@end
---
+++ ios/BeaconGuide/BeaconGuide/categoryTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  categoryTableViewCell.m
+//  BeaconGuide
+//
+//  Created by Kaili An on 8/2/14.
+//  Copyright (c) 2014 Beacon_Guide. All rights reserved.
+//
+
+#import "categoryTableViewCell.h"
+
+@implementation categoryTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
