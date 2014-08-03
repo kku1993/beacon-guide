68f21492f4fb02eec9702aac8a00866ea035eed2
Kelly An <4ever.kellyan@gmail.com>
2014-08-03 04:05:06 -0700

    update mapviewcontroller
    it can display indoor map with floor selection now

--- ios/BeaconGuide/BeaconGuide/MapViewController.m
+++ ios/BeaconGuide/BeaconGuide/MapViewController.m
@@ -10,17 +10,21 @@
 #import "GoogleMaps/GoogleMaps.h"
 
 
-@interface MapViewController ()
+@interface MapViewController ()< GMSIndoorDisplayDelegate,
+UIPickerViewDelegate,
+UIPickerViewDataSource>
 
 @end
 
 @implementation MapViewController{
     GMSMapView *mapView_;
-    NSArray *displays_; //array of JSON exhibit data
-    NSDictionary *display_;
-    GMSMarker *marker_;
-    NSDictionary *levels_;
+//    NSArray *displays_; //array of JSON exhibit data
+//    NSDictionary *display_;
+//    GMSMarker *marker_;
     
+    UIPickerView *levelPickerView_;
+//    NSDictionary *levels_;
+    NSArray *levels_;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -36,88 +40,74 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:37.38 longitude:-122.06 zoom:17];
+    
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:37.78318
+                                                            longitude:-122.403874
+                                                                 zoom:18];
+    
+    // set backgroundColor, otherwise UIPickerView fades into the background
+    self.view.backgroundColor = [UIColor grayColor];
+    
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.settings.myLocationButton = NO;
-    mapView_.settings.indoorPicker = NO;
-    mapView_.delegate = self;
+    mapView_.settings.indoorPicker = NO; // We are implementing a custom level picker.
+    
+    mapView_.indoorEnabled = YES; // Defaults to YES. Set to NO to hide indoor maps.
     mapView_.indoorDisplay.delegate = self;
+    mapView_.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:mapView_];
     
-    self.view = mapView_;
+    // This UIPickerView will be populated with the levels of the active building.
+    levelPickerView_ = [[UIPickerView alloc] init];
+    levelPickerView_.delegate = self;
+    levelPickerView_.dataSource = self;
+    levelPickerView_.showsSelectionIndicator = YES;
+    levelPickerView_.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:levelPickerView_];
     
-    //Parsing json data to map
-    NSString *jsonPath = [[NSBundle mainBundle]pathForResource:@"indoorMap" ofType:@"json"];
-    NSData *data = [NSData dataWithContentsOfFile:jsonPath];
-    displays_ = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+    // The height of the UIPickerView, used below in the vertical constraint
+    NSDictionary *metrics = @{@"height": @180.0};
+    NSDictionary *views = NSDictionaryOfVariableBindings(mapView_, levelPickerView_);
     
-    UISegmentedControl *segmentedControl = [[UISegmentedControl alloc]init];
-    [segmentedControl setSegmentedControlStyle:UISegmentedControlStyleBar];
-    [segmentedControl setTintColor:[UIColor colorWithRed:0.333f green:0.666f blue:0.882f alpha:.7f]];
+    // Constraining the map to the full width of the display.
+    // The |levelPickerView_| is constrained below with the NSLayoutFormatAlignAll*
+    // See http://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/AutolayoutPG/Articles/formatLanguage.html
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"|[mapView_]|"
+                               options:0
+                               metrics:metrics
+                               views:views]];
     
-    segmentedControl.translatesAutoresizingMaskIntoConstraints = NO;
-    [segmentedControl addTarget:self action:@selector(displaysSelected:) forControlEvents:UIControlEventValueChanged];
-    [self.view addSubview:segmentedControl];
-    
-    for(NSDictionary *display in displays_){
-        [segmentedControl insertSegmentWithImage:[UIImage imageNamed:display[@"key"]] atIndex:[displays_ indexOfObject:display] animated:NO];
-    }
-    
-    NSDictionary *views = NSDictionaryOfVariableBindings(segmentedControl);
-    
-    [self.view addConstraint:[NSLayoutConstraint
-                              constraintsWithVisualFormat:@"[segmentedControl]-|" options:kNilOptions metrics:nil views:views ]];
-    
-    
-    [self.view addConstraint:[NSLayoutConstraint
-                              constraintsWithVisualFormat:@"V: [segmentedControl]-|" options:kNilOptions metrics:nil views:views ]];
-    
-    
+    // Constraining the mapView_ and the levelPickerView_ as siblings taking
+    // the full height of the display, with levelPickerView_ at 200 points high
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"V:|[mapView_][levelPickerView_(height)]|"
+                               options:NSLayoutFormatAlignAllLeft|NSLayoutFormatAlignAllRight
+                               metrics:metrics
+                               views:views]];
     
 }
 
-- (void)moveMarker {
-    CLLocationCoordinate2D location = CLLocationCoordinate2DMake([display_[@"lat"] doubleValue], [display_[@"lng"] doubleValue]);
+
+- (void)didChangeActiveBuilding:(GMSIndoorBuilding *)building {
+    // Everytime we change active building force the picker to re-display the labels.
     
-    if(marker_ == nil){
-        marker_ = [GMSMarker markerWithPosition:location];
-        marker_.map = mapView_;
-    } else {
-        marker_.position = location;
+    NSMutableArray *levels = [NSMutableArray array];
+    if (building.underground) {
+        // If this building is completely underground, add a fake 'top' floor. This must be the 'boxed'
+        // nil, [NSNull null], as NSArray/NSMutableArray cannot contain nils.
+        [levels addObject:[NSNull null]];
     }
+    [levels addObjectsFromArray:building.levels];
+    levels_ = [levels copy];
     
-    marker_.title = display_[@"name"];
-    [mapView_ animateToLocation:location];
-    [mapView_ animateToZoom:19];
+    [levelPickerView_ reloadAllComponents];
+    [levelPickerView_ selectRow:-1 inComponent:0 animated:NO];
+    
+    // UIPickerView insists on having some data; disable interaction if there's no levels.
+    levelPickerView_.userInteractionEnabled = ([levels_ count] > 0);
 }
 
-- (void)displaysSelected: (UISegmentedControl *)segmentedControl {
-    display_ = displays_[[segmentedControl selectedSegmentIndex]];
-    [self moveMarker];
-}
-
-- (void)mapView: (GMSMapView *)mapView idleAtCameraPosition:(GMSCameraPosition *)camera {
-    if(display_ != nil){
-        CLLocationCoordinate2D location = CLLocationCoordinate2DMake([display_[@"lat"] doubleValue], [display_[@"lng"] doubleValue]);
-        if([mapView_.projection containsCoordinate:location] && levels_ != nil){
-            [mapView.indoorDisplay setActiveLevel:levels_[display_[@"level"]]];
-        }
-    }
-}
-
-- (void)didChangeActiveBuilding: (GMSIndoorBuilding *)building{
-    if(building != nil){
-        NSMutableDictionary *levels = [NSMutableDictionary dictionary];
-        for(GMSIndoorLevel *level in building.levels){
-            [levels setObject:level forKey:level.shortName];
-        }
-        
-        levels_ = [NSDictionary dictionaryWithDictionary:levels];
-    }
-    else {
-        levels_ = nil;
-    }
-    
-}
 
 - (void)didReceiveMemoryWarning
 {
@@ -126,27 +116,53 @@
 }
 
 
+- (void)didChangeActiveLevel:(GMSIndoorLevel *)level {
+    // On level change, sync our level picker's selection to the IndoorDisplay.
+    if (level == nil) {
+        level = (id)[NSNull null];  // box nil to NSNull for use in NSArray
+    }
+    NSUInteger index = [levels_ indexOfObject:level];
+    if (index != NSNotFound) {
+        NSInteger currentlySelectedLevel = [levelPickerView_ selectedRowInComponent:0];
+        if ((NSInteger)index != currentlySelectedLevel) {
+            [levelPickerView_ selectRow:index inComponent:0 animated:NO];
+        }
+    }
+}
 
-//- (GMSMapView *)mapView{
-//    
-//    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:37.38 longitude:-122.06 zoom:6];
-//    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-//    mapView_.myLocationEnabled = YES;
-//    self.view = mapView_;
-//    
-//    //create a market in the center of the map
-//    GMSMarker *marker = [[GMSMarker alloc]init];
-//    marker.position = CLLocationCoordinate2DMake(37.38, -122.06);
-//    marker.title = @"Mountain View";
-//    marker.snippet = @"California, CA";
-//    marker.map = mapView_;
-    
-//    if(!_mapView){
-//        GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:37.38 longitude:-122.06 zoom:6];
-//        _mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-//        _mapView.buildingsEnabled = NO;
-//        _mapView.myLocationEnabled = YES;
-//    }
-//    return _mapView;
-//}
+#pragma mark - UIPickerViewDelegate
+
+- (void)pickerView:(UIPickerView *)pickerView
+      didSelectRow:(NSInteger)row
+       inComponent:(NSInteger)component {
+    // On user selection of a level in the picker, set the right level in IndoorDisplay
+    id level = levels_[row];
+    if (level == [NSNull null]) {
+        level = nil;  // unbox NSNull
+    }
+    [mapView_.indoorDisplay setActiveLevel:level];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView
+             titleForRow:(NSInteger)row
+            forComponent:(NSInteger)component {
+    id object = levels_[row];
+    if (object == [NSNull null]) {
+        return @"\u2014";  // use an em dash for 'above ground'
+    }
+    GMSIndoorLevel *level = object;
+    return level.name;
+}
+
+#pragma mark - UIPickerViewDataSource
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return [levels_ count];
+}
+
+
 @end
