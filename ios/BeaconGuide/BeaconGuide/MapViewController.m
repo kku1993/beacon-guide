ca7f132eb55503185432981c31d8b1aa41ab3359
Kelly An <4ever.kellyan@gmail.com>
2014-08-03 02:18:48 -0700

    remove googlemap from pod

--- ios/BeaconGuide/BeaconGuide/MapViewController.m
+++ ios/BeaconGuide/BeaconGuide/MapViewController.m
@@ -9,14 +9,14 @@
 #import "MapViewController.h"
 #import "GoogleMaps/GoogleMaps.h"
 
+
 @interface MapViewController ()
+@property (nonatomic, strong) GMSMapView *mapView;
 
 @end
 
 @implementation MapViewController
-{
-    GMSMapView *mapView_;
-}
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -30,17 +30,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:37.38 longitude:-122.06 zoom:6];
-    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-    mapView_.myLocationEnabled = YES;
-    self.view = mapView_;
-    
-    //create a market in the center of the map
-    GMSMarker *marker = [[GMSMarker alloc]init];
-    marker.position = CLLocationCoordinate2DMake(37.38, -122.06);
-    marker.title = @"Mountain View";
-    marker.snippet = @"California, CA";
-    marker.map = mapView_;
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.mapView];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,4 +41,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+- (GMSMapView *)mapView{
+    
+//    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:37.38 longitude:-122.06 zoom:6];
+//    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+//    mapView_.myLocationEnabled = YES;
+//    self.view = mapView_;
+//    
+//    //create a market in the center of the map
+//    GMSMarker *marker = [[GMSMarker alloc]init];
+//    marker.position = CLLocationCoordinate2DMake(37.38, -122.06);
+//    marker.title = @"Mountain View";
+//    marker.snippet = @"California, CA";
+//    marker.map = mapView_;
+    
+    if(!_mapView){
+        GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:37.38 longitude:-122.06 zoom:6];
+        _mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+        _mapView.buildingsEnabled = NO;
+        _mapView.myLocationEnabled = YES;
+    }
+    return _mapView;
+}
 @end
